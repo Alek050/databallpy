d72867a9e90c759eb407b926893248c7e6a14103
Daan <daaniogrob@gmail.com>
2023-01-26 14:53:58 +0100

    Added Metadata class

---
+++ databallpy/load_data/metadata.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Optional
+import pandas as pd
+import numpy as np
+
+@dataclass
+class Metadata:
+    match_id: int
+    pitch_size_x_m: float
+    pitch_size_y_m: float
+    match_start_datetime: np.datetime64
+    periods_frames: pd.DataFrame
+
+    frame_rate: int
+
+    home_team_id: int
+    home_team_name: str
+    home_players: pd.DataFrame
+    home_score: int
+    home_formation: str
+
+    away_team_id: int
+    away_team_name: str
+    away_players: pd.DataFrame
+    away_score: int
+    away_formation: str
