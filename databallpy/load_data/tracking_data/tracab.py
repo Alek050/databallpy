cf7bfb238c6175dd1bba18a43551f62f53a31dee
Daan <daaniogrob@gmail.com>
2023-02-02 11:33:45 +0100

    fixed comments on functions

--- databallpy/load_data/tracking_data/tracab.py
+++ databallpy/load_data/tracking_data/tracab.py
@@ -7,24 +7,26 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from databallpy.load_data.metadata import Metadata
+from databallpy.load_data.tracking_data._add_player_data_to_dict import _add_player_data_to_dict
+from databallpy.load_data.tracking_data._add_ball_data_to_dict import _add_ball_data_to_dict
+from databallpy.load_data.tracking_data._insert_missing_rows import _insert_missing_rows
 
-
-def load_tracking_data_tracab(
+def load_tracab_tracking_data(
     tracab_loc: str, meta_data_loc: str, verbose: bool = True
 ) -> Tuple[pd.DataFrame, Metadata]:
-    """Function to load tracking data and metadata in the tracab format
+    """Function to load tracking data and metadata from the tracab format
 
     Args:
         tracab_loc (str): location of the tracking_data.dat file
-        meta_data-loc (str): location of the meta_data.xml file
-        verbose (bool): whether to print info, defaults to True
+        meta_data_loc (str): location of the meta_data.xml file
+        verbose (bool): whether to print on progress of loading in the terminal , defaults to True
 
     Returns:
-        Tuple[pd.DataFrame, Metadata], the tracking data and metadate class
+        Tuple[pd.DataFrame, Metadata], the tracking data and metadata class
     """
 
     tracking_data = _get_tracking_data(tracab_loc, verbose)
-    meta_data = _get_meta_data(meta_data_loc)
+    meta_data = _get_metadata(meta_data_loc)
 
     return tracking_data, meta_data
 
@@ -49,7 +51,6 @@ def _get_tracking_data(tracab_loc: str, verbose: bool) -> pd.DataFrame:
         print(" - Completed")
 
     file.close()
-
     size_lines = len(lines)
 
     data = {
@@ -57,122 +58,39 @@ def _get_tracking_data(tracab_loc: str, verbose: bool) -> pd.DataFrame:
         "ball_x": [np.nan] * size_lines,
         "ball_y": [np.nan] * size_lines,
         "ball_z": [np.nan] * size_lines,
-        "ball_speed": [np.nan] * size_lines,
         "ball_status": [None] * size_lines,
         "ball_posession": [None] * size_lines,
     }
 
     if verbose:
         print("Writing lines to dataframe:")
+        lines = tqdm(lines)
 
-    for idx, line in enumerate(tqdm(lines)):
+    for idx, line in enumerate(lines):
 
         timestamp, players_info, ball_info, _ = line.split(":")
         data["timestamp"][idx] = int(timestamp)
 
         players = players_info.split(";")[:-1]
         for player in players:
-            data = _add_player_data_to_dict(player, data, idx)
+            team_id, _, shirt_num, x, y, _ = player.split(",")
+            data = _add_player_data_to_dict(team_id, shirt_num, x, y, data, idx)
 
-        data = _add_ball_data_to_dict(ball_info, data, idx)
+        ball_x, ball_y, ball_z, _, posession, status = ball_info.split(";")[0].split(",")[:6]
+        data = _add_ball_data_to_dict(ball_x, ball_y, ball_z, posession, status, data, idx)
 
     df = pd.DataFrame(data)
 
     for col in df.columns:
-        if "_x" in col or "_y" in col or "_z" in col or "_speed" in col:
-            df[col] /= 100
-            df[col] = np.round(df[col], 3)
-        else:
-            pass
+        if "_x" in col or "_y" in col or "_z" in col:
+            df[col] = np.round(df[col]/100, 3) #change cm to m
 
-    df = _insert_missing_rows(df)
+    df = _insert_missing_rows(df, "timestamp")
 
     return df
 
 
-def _add_player_data_to_dict(player: str, data: dict, idx: int) -> dict:
-    """Function that adds the data of one player to the data dict for one frame
-
-    Args:
-        player (str): containing data from player
-        data (dict): data dictionary to write results to
-        idx (int): indicates position in data dictionary
-
-    Returns:
-        dict: contains all tracking data
-    """
-
-    team_id, _, shirt_num, x, y, speed = player.split(",")
-
-    team_ids = {0: "home", 1: "away"}
-    team = team_ids.get(int(team_id))
-    if team is None:  # player is unknown or referee
-        return data
-
-    if f"{team}_{shirt_num}_x" not in data.keys():  # create keys for new player
-        data[f"{team}_{shirt_num}_x"] = [np.nan] * len(data["timestamp"])
-        data[f"{team}_{shirt_num}_y"] = [np.nan] * len(data["timestamp"])
-        data[f"{team}_{shirt_num}_speed"] = [np.nan] * len(data["timestamp"])
-
-    data[f"{team}_{shirt_num}_x"][idx] = int(x)
-    data[f"{team}_{shirt_num}_y"][idx] = int(y)
-    data[f"{team}_{shirt_num}_speed"][idx] = float(speed)
-
-    return data
-
-
-def _add_ball_data_to_dict(ball_info: str, data: dict, idx: str) -> dict:
-    """Function that adds the data of the ball to the data dict for one frame
-
-    Args:
-        ball_info (str): containing data from the ball
-        data (dict): data dictionary to write results to
-        idx (int): indicates position in data dictionary
-
-    Returns:
-        dict: contains all tracking data
-    """
-
-    x, y, z, speed, posession, status = ball_info.split(";")[0].split(",")[:6]
-    data["ball_x"][idx] = int(x)
-    data["ball_y"][idx] = int(y)
-    data["ball_z"][idx] = int(z)
-    data["ball_speed"][idx] = float(speed)
-    data["ball_posession"][idx] = posession
-    data["ball_status"][idx] = status
-
-    return data
-
-
-def _insert_missing_rows(df: pd.DataFrame) -> pd.DataFrame:
-    """Functions that inserts missing rows based on gaps in timestamp
-
-    Args:
-        df (pd.DataFrame): containing tracking data
-
-    Returns:
-        pd.DataFrame: contains tracking data with inserted missing rows
-    """
-    assert (
-        "timestamp" in df.columns
-    ), "Calculations are based on timestamp column, which is not in the df"
-
-    missing = np.where(df["timestamp"].diff() > 1)[0]
-    for start_missing in missing:
-        n_missing = int(df["timestamp"].diff()[start_missing] - 1)
-        start_timestamp = df.loc[start_missing, "timestamp"] - n_missing
-        to_add_data = {
-            "timestamp": list(np.arange(start_timestamp, start_timestamp + n_missing))
-        }
-        to_add_df = pd.DataFrame(to_add_data)
-        df = pd.concat((df, to_add_df)).sort_values(by="timestamp")
-
-    df.reset_index(drop=True, inplace=True)
-
-    return df
-
-
-def _get_meta_data(meta_data_loc: str) -> Metadata:
+def _get_metadata(metadata_loc: str) -> Metadata:
     """Function that reads metadata.xml file and stores it in Metadata class
 
     Args:
@@ -182,7 +100,7 @@ def _get_meta_data(meta_data_loc: str) -> Metadata:
         Metadata: class that contains metadata
     """
 
-    file = open(meta_data_loc, "r")
+    file = open(metadata_loc, "r")
     lines = file.read()
     lines = lines.replace("ï»¿", "")
     soup = BeautifulSoup(lines, "xml")
@@ -204,14 +122,28 @@ def _get_meta_data(meta_data_loc: str) -> Metadata:
     home_team = soup.find("HomeTeam")
     home_team_name = home_team.find("LongName").text
     home_team_id = int(home_team.find("TeamId").text)
-    df_home_players = _get_player_data(home_team)
+    
+    home_players_info = []
+    for player in home_team.find_all("Player"):
+        player_dict = {}
+        for element in player.findChildren():
+            player_dict[element.name] = element.text
+        home_players_info.append(player_dict)
+    df_home_players = _get_player_data(home_players_info)
 
     away_team = soup.find("AwayTeam")
     away_team_name = away_team.find("LongName").text
     away_team_id = int(away_team.find("TeamId").text)
-    df_away_players = _get_player_data(away_team)
+    
+    away_players_info = []
+    for player in away_team.find_all("Player"):
+        player_dict = {}
+        for element in player.findChildren():
+            player_dict[element.name] = element.text
+        away_players_info.append(player_dict)
+    df_away_players = _get_player_data(away_players_info)
 
-    meta_data = Metadata(
+    metadata = Metadata(
         match_id=match_id,
         pitch_dimensions=[pitch_size_x, pitch_size_y],
         match_start_datetime=match_start_datetime,
@@ -220,25 +152,25 @@ def _get_meta_data(meta_data_loc: str) -> Metadata:
         home_team_id=home_team_id,
         home_team_name=home_team_name,
         home_players=df_home_players,
-        home_score=None,
+        home_score=np.nan,
         home_formation=None,
         away_team_id=away_team_id,
         away_team_name=away_team_name,
         away_players=df_away_players,
-        away_score=None,
+        away_score=np.nan,
         away_formation=None,
     )
 
     file.close()
 
-    return meta_data
+    return metadata
 
 
-def _get_player_data(team: bs4.element.Tag) -> pd.DataFrame:
+def _get_player_data(players_info: bs4.element.Tag) -> pd.DataFrame:
     """Function that creates a df containing info on all players for a team
 
     Args:
-        team (bs4.element.Tag): containing info no all players of a team
+        team (bs4.element.Tag): containing info on all players of a team
 
     Returns:
         pd.DataFrame: contains all player information for a team
@@ -251,14 +183,14 @@ def _get_player_data(team: bs4.element.Tag) -> pd.DataFrame:
         "start_frame": [],
         "end_frame": [],
     }
-    for player in team.find("Players").find_all("Player"):
-        player_dict["id"].append(int(player.find("PlayerId").text))
+    for player in players_info:
+        player_dict["id"].append(player["PlayerId"])
         player_dict["full_name"].append(
-            player.find("FirstName").text + " " + player.find("LastName").text
+            player["FirstName"] + " " + player["LastName"]
         )
-        player_dict["shirt_num"].append(int(player.find("JerseyNo").text))
-        player_dict["start_frame"].append(int(player.find("StartFrameCount").text))
-        player_dict["end_frame"].append(int(player.find("EndFrameCount").text))
+        player_dict["shirt_num"].append(int(player["JerseyNo"]))
+        player_dict["start_frame"].append(int(player["StartFrameCount"]))
+        player_dict["end_frame"].append(int(player["EndFrameCount"]))
     df = pd.DataFrame(player_dict)
 
     return df
