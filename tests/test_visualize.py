f1e1ac0dfc702d09abaa046fcac4f6c7e1ddc782
Alexander Oonk <alexanderoonk26@gmail.com>
2023-02-16 18:41:53 +0100

    linters....

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,10 +1,11 @@
-import unittest
 import os
+import unittest
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from databallpy.visualize import plot_soccer_pitch, requires_ffmpeg, save_match_clip
 from databallpy.match import get_match
+from databallpy.visualize import plot_soccer_pitch, requires_ffmpeg, save_match_clip
 
 
 class TestVisualize(unittest.TestCase):
@@ -27,7 +28,7 @@ class TestVisualize(unittest.TestCase):
         self.assertIsInstance(ax, plt.Axes)
         self.assertEqual(len(ax.lines), 27)
         self.assertEqual(len(ax.collections), 3)
-    
+
     def test_save_match_clip(self):
         match = get_match(
             tracking_data_loc="tests/test_data/tracab_td_test.dat",
@@ -35,19 +36,37 @@ class TestVisualize(unittest.TestCase):
             tracking_data_provider="tracab",
             event_data_loc="tests/test_data/f24_test.xml",
             event_metadata_loc="tests/test_data/f7_test.xml",
-            event_data_provider="opta"
+            event_data_provider="opta",
         )
         series = pd.Series([22, 23, 25], index=[1, 2, 3])
         with self.assertRaises(AssertionError):
-            save_match_clip(match, 1, 3, save_folder="tests/test_data", title="test_clip", events=["pass"])
-            save_match_clip(match, 0, 2, save_folder="tests/test_data", title="test_clip", variable_of_interest=series)
-        
+            save_match_clip(
+                match,
+                1,
+                3,
+                save_folder="tests/test_data",
+                title="test_clip",
+                events=["pass"],
+            )
+            save_match_clip(
+                match,
+                0,
+                2,
+                save_folder="tests/test_data",
+                title="test_clip",
+                variable_of_interest=series,
+            )
+
         assert not os.path.exists("tests/test_data/test_clip.mp4")
 
-        save_match_clip(match, 1, 3, save_folder="tests/test_data", title="test_clip", variable_of_interest=series)
+        save_match_clip(
+            match,
+            1,
+            3,
+            save_folder="tests/test_data",
+            title="test_clip",
+            variable_of_interest=series,
+        )
 
         assert os.path.exists("tests/test_data/test_clip.mp4")
         os.remove("tests/test_data/test_clip.mp4")
-
-        
-
