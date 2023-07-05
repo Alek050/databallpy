cfe7b42e1f82b87542f19d9d4c474d5e28aa4c97
Daan <daaniogrob@gmail.com>
2023-07-05 15:33:45 +0200

    Added moving average filter

---
+++ databallpy/filters.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+
+def filter_data(
+    df: pd.DataFrame,
+    input_columns: list,
+    kind: str = "moving_average",
+    window_length: int = 5,
+) -> pd.DataFrame:
+    """Function to filter specified columns, available filters are: moving average
+
+    Args:
+        df (pd.DataFrame): dataframe with columns that need to be filtered
+        input_columns (list): columns that need to be filtered
+        kind (str, optional): kind of filter to apply. One of: "moving_average"
+                              Defaults to "moving_average".
+        window_length (int, optional): window length to be used by the filter.
+                                       Applies to the following filters:
+                                       "moving_average".
+                                       Defaults to 5.
+
+    Returns:
+        pd.DataFrame: dataframe with filtered columns
+    """
+    # Check input types
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError(f"df should be a pd.DataFrame, not a {type(df)}")
+
+    if not isinstance(input_columns, list):
+        raise TypeError(f"input_columns should be a list, not a {type(input_columns)}")
+
+    if not all(isinstance(col, str) for col in input_columns):
+        raise TypeError("All elements of input_columns should be strings")
+
+    if not isinstance(kind, str):
+        raise TypeError(f"kind should be a string, not a {type(kind)}")
+
+    filter_kind_list = ["moving_average"]
+    if kind not in filter_kind_list:
+        raise TypeError(
+            f"kind should be one of {(', '.join(filter_kind_list))}, not {kind}"
+        )
+
+    if not isinstance(window_length, int):
+        raise TypeError(f"window_length should be an int, not a {type(window_length)}")
+
+    if kind == "moving_average":
+        for col in input_columns:
+            df[col] = df[col].rolling(window_length).mean()
+
+    return df
---
+++ tests/test_filters.py
@@ -0,0 +1,109 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+
+from databallpy.filters import filter_data
+
+
+class TestFilters(unittest.TestCase):
+    def setUp(self):
+        self.input = pd.DataFrame(
+            {
+                "col_1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                "col_2": [5, 6, -1, -9, 10, 11, 12, -50, 1, 3],
+            }
+        )
+
+    def test_moving_average_filter(self):
+        expected_output_1 = pd.DataFrame(
+            {
+                "col_1": [np.nan, np.nan, np.nan, np.nan, 3, 4, 5, 6, 7, 8],
+                "col_2": [
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    2.2,
+                    3.4,
+                    4.6,
+                    -5.2,
+                    -3.2,
+                    -4.6,
+                ],
+            }
+        )
+
+        output_1 = filter_data(self.input, ["col_1", "col_2"])
+        pd.testing.assert_frame_equal(expected_output_1, output_1)
+
+    def test_single_column_filter(self):
+        expected_output_2 = pd.DataFrame(
+            {
+                "col_1": [np.nan, np.nan, np.nan, np.nan, 3, 4, 5, 6, 7, 8],
+                "col_2": [5, 6, -1, -9, 10, 11, 12, -50, 1, 3],
+            }
+        )
+
+        output_2 = filter_data(self.input, ["col_1"])
+        pd.testing.assert_frame_equal(expected_output_2, output_2)
+
+    def test_different_window_moving_average(self):
+        expected_output_3 = pd.DataFrame(
+            {
+                "col_1": [np.nan, np.nan, np.nan, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5],
+                "col_2": [
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    0.25,
+                    1.50,
+                    2.75,
+                    6,
+                    -4.25,
+                    -6.5,
+                    -8.5,
+                ],
+            }
+        )
+
+        output_3 = filter_data(self.input, ["col_1", "col_2"], window_length=4)
+        pd.testing.assert_frame_equal(expected_output_3, output_3)
+
+    def test_filters_wrong_input(self):
+
+        with self.assertRaises(TypeError) as cm:
+            filter_data({"key": "value"}, ["key"])
+        self.assertEqual(
+            str(cm.exception), "df should be a pd.DataFrame, not a <class 'dict'>"
+        )
+
+        with self.assertRaises(TypeError) as cm:
+            filter_data(self.input, "col_1")
+        self.assertEqual(
+            str(cm.exception), "input_columns should be a list, not a <class 'str'>"
+        )
+
+        with self.assertRaises(TypeError) as cm:
+            filter_data(self.input, ["col_1", 2])
+        self.assertEqual(
+            str(cm.exception), "All elements of input_columns should be strings"
+        )
+
+        with self.assertRaises(TypeError) as cm:
+            filter_data(self.input, ["col_1", "col_2"], kind=5)
+        self.assertEqual(
+            str(cm.exception), "kind should be a string, not a <class 'int'>"
+        )
+
+        with self.assertRaises(TypeError) as cm:
+            filter_data(self.input, ["col_1", "col_2"], kind="magic_filter")
+        self.assertEqual(
+            str(cm.exception), "kind should be one of moving_average, not magic_filter"
+        )
+
+        with self.assertRaises(TypeError) as cm:
+            filter_data(self.input, ["col_1", "col_2"], "moving_average", "5")
+        self.assertEqual(
+            str(cm.exception), "window_length should be an int, not a <class 'str'>"
+        )
