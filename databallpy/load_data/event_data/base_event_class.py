a8f3d29fe8e8bcc359a7f89e21744869dd6403df
Daan <daaniogrob@gmail.com>
2023-08-17 14:44:09 +0200

    Created base event class

---
+++ databallpy/load_data/event_data/base_event_class.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+import pandas as pd
+
+
+@dataclass
+class Event(ABC):
+    """This is the base event class from which the specific event classes are inherited.
+    It containts all the basic information that is available for every event.
+
+    Args:
+        event_id (int): distinct id of the event
+        period_id (int): id of the period
+        minutes (int): minute in which the event occurs
+        seconds (int): seconds within the aforementioned minute where the event occurs
+        datetime (pd.Timestamp): datetime at which the event occured
+    """
+
+    event_id: int
+    period_id: int
+    minutes: int
+    seconds: int
+    datetime: pd.Timestamp
+    x_start: float
+    y_start: float
+
+    def __post_init__(self):
+        if not isinstance(self.event_id, int):
+            raise TypeError(f"event_id should be int, not {type(self.event_id)}")
+
+        if not isinstance(self.period_id, int):
+            raise TypeError(f"period_id should be int, not {type(self.period_id)}")
+
+        if not isinstance(self.minutes, int):
+            raise TypeError(f"minutes should be int, not {type(self.minutes)}")
+
+        if not isinstance(self.seconds, int):
+            raise TypeError(f"seconds should be int, not {type(self.seconds)}")
+
+        if not isinstance(self.datetime, pd.Timestamp):
+            raise TypeError(
+                f"datetime should be pd.Timestamp, not {type(self.datetime)}"
+            )
+
+        if not isinstance(self.x_start, float):
+            raise TypeError(f"x_start should be a float, not {type(self.x_start)}")
+
+        if not isinstance(self.y_start, float):
+            raise TypeError(f"y_start should be a float, not {type(self.y_start)}")
+
+    @abstractmethod
+    def to_dataframe(self):
+        pass
