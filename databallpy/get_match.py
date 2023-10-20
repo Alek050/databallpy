929c944adb11ccad1fe0cb10327f76d962e76b93
Alexander Oonk <49450063+Alek050@users.noreply.github.com>
2023-10-20 15:21:09 +0200

    Changed max timestmap difference for sync (#160)
    * Changed max timestmap difference for sync
    
    * change .pickle extension in get saved match

--- databallpy/get_match.py
+++ databallpy/get_match.py
@@ -282,6 +282,8 @@ def get_saved_match(name: str, path: str = os.getcwd()) -> Match:
     Returns:
         Match: All information about the match
     """
+    if name[-7:] == ".pickle":
+        name = name[:-7]
     with open(os.path.join(path, name + ".pickle"), "rb") as f:
         match = pickle.load(f)
     return match
