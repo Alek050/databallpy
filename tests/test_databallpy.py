48b10070a57a1e9afdca990440208f98b5e58ee7
Alexander Oonk <alexanderoonk26@gmail.com>
2024-01-25 09:36:31 +0100

    commented test_get_match

--- tests/test_databallpy.py
+++ tests/test_databallpy.py
@@ -1,5 +1,5 @@
-# from databallpy import __version__
+from databallpy import __version__
 
 
-# def test_version():
-#     assert __version__ == "0.4.3"
+def test_version():
+    assert __version__ == "0.4.3"
