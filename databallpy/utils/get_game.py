9905424ed63f43576b962c45e9afd7e9b8e8d42c
Alexander Oonk <alexanderoonk26@gmail.com>
2026-04-01 15:10:07 +0200

    try except in get saved game when cache is true

--- databallpy/utils/get_game.py
+++ databallpy/utils/get_game.py
@@ -654,7 +654,10 @@ def try_cache(provider: str, game_id: str) -> Game | None:
                 shutil.copy(file, cache_path)
 
     if cache_path.is_dir():
-        return get_saved_game(cache_path)
+        try:
+            return get_saved_game(cache_path)
+        except FileNotFoundError:
+            pass
     return None
 
 
